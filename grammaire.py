fefe9d654bc782ff8c97c81a9c583d2f421de2dc
M3triXxX <strongheartgamingro@gmail.com>
2023-05-02 15:22:41 +0200

    ..

--- grammaire.py
+++ grammaire.py
@@ -55,10 +55,8 @@ dumbo_grammar = """
     variable: ("_"|"a"|"b"|"c"|"d"|"e"|"f"|"g"|"h"|"i"|"j"|"k"|"l"|"m"|"n"|"o"|"p"|"q"|"r"|"s"|"t"|"u"|"v"|"w"|"x"|"y"|"z") ("_"|"a"|"b"|"c"|"d"|"e"|"f"|"g"|"h"|"i"|"j"|"k"|"l"|"m"|"n"|"o"|"p"|"q"|"r"|"s"|"t"|"u"|"v"|"w"|"x"|"y"|"z"|"0"|"1"|"2"|"3"|"4"|"5"|"6"|"7"|"8"|"9")*
     string: "'" charactere* "'"
 
-    %import common.CNAME -> NAME
-    %import common.NUMBER
+    
     %import common.WS_INLINE
-
     %ignore WS_INLINE
 """
 

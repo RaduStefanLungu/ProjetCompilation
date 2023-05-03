a9df6c78f145dfd4dee7bf8fc47e7856896b17ba
M3triXxX <strongheartgamingro@gmail.com>
2023-05-03 11:43:18 +0200

    Grammaire +- finie?

--- grammaire.py
+++ grammaire.py
@@ -32,13 +32,15 @@ except NameError:
 # </body>
 # </html>
 
+#TODO : completer la liste de tt les characteres
+
 dumbo_grammar = """
     programme: txt
         | txt programme2
         | programme2
     programme2: dumbo_bloc | dumbo_bloc programme
     txt: charactere*
-    charactere: /[A-Za-z_0-9]/ | /</ | />/ | /:/ | /\//
+    charactere: /[A-Za-z_0-9]/ | /</ | />/ | /:/ | /\// | /=/ | /"/ | /,/
     dumbo_bloc: "{{" expressions_list "}}"
     expressions_list: expression ";" expressions_list
         | expression ";"
@@ -47,17 +49,20 @@ dumbo_grammar = """
         | "for" variable "in" variable "do" expressions_list "endfor"
         | variable ":=" string_expression
         | variable ":=" string_list
+        | variable ":=" numbers
     string_expression: string
         | variable
         | string_expression "." string_expression
     string_list: "(" string_list_interior ")"
     string_list_interior: string | string "," string_list_interior
-    variable: ( /_/ | /[A-Za-z]/) /[A-Za-z]/*
+    variable: ( /_/ | /[A-Za-z]/) /[A-Za-z_0-9]/*
     string: "'" charactere* "'"
-
+    numbers: integer | float
+    float: integer+ "." integer*
+    integer: /[0-9]/+
     
-    %import common.WS_INLINE
-    %ignore WS_INLINE
+    %import common.WS
+    %ignore WS
 """
 
 
@@ -71,13 +76,52 @@ def main():
 
 sentence = '''<html>Case:{{
             _Axel := ('Baby','Shark') ;
-            for _el in _Axel do 
-                print 'Ok';
-            endfor
-            }}</html>'''
+            _Jinx := _Axel;
+            i:= 0;
+            for nom in _Axel do
+                print '<a href="'.nom.'">'.nom.'</a>';
+            endfor;
+
+            }}</html>
+            '''
+
+sentence2 = '''
+    <html>
+        <head>
+            <title> {{ print nom.' '.prenom;}} </title>
+        </head>
+        <body>
+            <h1>
+                {{ print nom.' '.prenom;}}
+            </h1>
+            Cours: {{ 
+                for c in cours do
+                    print c.', ';
+                endfor;
+                }}
+        </body>
+    </html>
+'''
+
+sentence3 = '''
+        {{
+            nom := 'De Pril';
+            prenom := 'Julie';
+            cours := ('Math discretes');
+        }}
+
+'''
+
+ana_gram_1 = '''
+        {{
+        nom := 'Marry';
+        _Jinx := 69;
+        }}
+'''
+
 
 def test():
-    print(dumbo_parser.parse(sentence).pretty())
+    print(dumbo_parser.parse(ana_gram_1).pretty())
 
 if __name__ == '__main__':
     test()

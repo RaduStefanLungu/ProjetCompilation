3ef7e3cebf9115b55a7a61c661dbb9a8914f5097
M3triXxX <strongheartgamingro@gmail.com>
2023-05-04 16:01:44 +0200

    Done : Variable assignement | TODO : FIX PRINTER

--- grammaire.py
+++ grammaire.py
@@ -5,7 +5,7 @@ A simple example of a REPL calculator
 This example shows how to write a basic calculator with variables.
 """
 from lark import Lark, Transformer, v_args
-
+import lark
 
 try:
     input = raw_input   # For Python2 compatibility
@@ -47,7 +47,7 @@ dumbo_grammar = """
     expression: "print" string_expression                                   -> f_print
         | "for" variable "in" string_list "do" expressions_list "endfor"
         | "for" variable "in" variable "do" expressions_list "endfor"
-        | variable ":=" string_expression
+        | variable ":=" string_expression                                   -> f_assign_var
         | variable ":=" string_list
         | variable ":=" numbers
     string_expression: string
@@ -66,21 +66,80 @@ dumbo_grammar = """
 """
 
 
-class OptimusPrime(Transformer):
+# class OptimusPrime(Transformer):
     
-    def __init__(self):
-        self.vars={}
+#     def __init__(self):
+#         self.vars={}
 
-    def f_print(self,value):
-        print(value)
-        return value
+#     def f_print(self,value):
+#         print(value)
+#         return value
 
 
-dumbo_parser = Lark(dumbo_grammar,parser='lalr',start='programme', ambiguity='explicit',transformer=OptimusPrime())
+# dumbo_parser = Lark(dumbo_grammar,parser='lalr',start='programme', ambiguity='explicit',transformer=OptimusPrime())
+# dumbo = dumbo_parser.parse
+
+dumbo_parser = Lark(dumbo_grammar,start='programme', ambiguity='explicit')
 dumbo = dumbo_parser.parse
 
-def main():
-    print("%s","HELLOW")
+variables = {}
+PRINTER = "template"
+
+def run_instructions(t):
+
+    if t.data == 'f_print':
+        # print(string_constructor_2(t.iter_subtrees_topdown()))
+        temp = string_constructor_2(t.children[0].children[0].children)
+        if PRINTER != temp :
+            PRINTER = temp
+            print(PRINTER)
+        # print(t.children)
+
+    elif t.data == 'f_assign_var':
+        
+        the_variable_0 = t.children[0]
+        the_value_0 = t.children[1]
+
+        # Left here for debugging purposes.
+        the_variable = None
+        the_value = None
+
+        for x in the_variable_0.iter_subtrees():
+            if x.data == 'variable':
+                the_variable = string_constructor_from_token(x.children)
+
+        for x in the_value_0.iter_subtrees():
+            if x.data == 'string':
+                the_value = string_constructor_2(x.children)
+        
+            elif x.data == 'variable':
+                the_value = variables.get(string_constructor_from_token(x.children))
+
+        variables[the_variable] = the_value
+
+def string_constructor_2(list):
+    string = ""
+    for tree in list:
+        string += tree.children[0].value
+    return string
+
+def string_constructor_from_token(token_list):
+    string = ""
+    for token in token_list:
+        string += token.value
+    return string
+
+
+
+def run(program):
+    dumbo_tree = dumbo_parser.parse(program)
+    # dumbo_tree(program)
+
+
+    for inst in dumbo_tree.iter_subtrees_topdown():
+        run_instructions(inst)
+    print(variables)
+
 
 sentence = '''<html>Case:{{
             _Axel := ('Baby','Shark') ;
@@ -123,18 +182,12 @@ sentence3 = '''
 ana_gram_1 = '''
         {{
         nom := 'Marry';
+        my_var := nom;
         print 'JINXED' ;
         _Jinx := 69;
         }}
 '''
 
-def run(program):
-    dumbo_tree = dumbo_parser.parse
-    dumbo_tree(program)
-
-def test_special(program):
-    run(program)
-
 if __name__ == '__main__':
-    test_special(ana_gram_1)
+    run(ana_gram_1)
     # main()
